7cae2d312443092a364dbd154aba24153e04d742
wzx <437686321@qq.com>
2022-10-08 16:57:28 +0800

    '1111

--- src/views/AttendanceSettings/AttendanceSettings.jsx
+++ src/views/AttendanceSettings/AttendanceSettings.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
-import './AttendanceSettings.scss'
+import React from 'react';
+import './AttendanceSettings.scss';
+import { Input, Button } from 'antd';
 
 export default function AttendanceSettings() {
   return (
     <div>
-        <h1>考勤设置</h1>
-        Attendance Settings
+       <div className='num'>
+        <h3>数据筛选</h3>
+        <div style={{display: 'flex', alignItems: 'center', height: '90px', paddingLeft: '50px'}}>
+          <Input addonBefore='输入查询:' style={{width: '300px'}}></Input>
+          <Input addonBefore='时间:' style={{width: '300px', marginLeft: '50px'}}></Input>
+          <Button type="primary"  style={{marginLeft: '20px'}}>
+            查询
+          </Button>
+          <Button style={{marginLeft: '20px'}}>
+            重置
+          </Button>
+        </div>
+       </div>
     </div>
   )
 }
